efdb4139092354a0316e0a2aa0b4b3a526433f6d
raheem <raheemideadev@gmail.com>
2025-08-03 14:28:57 +0500

    [1.0.12] Implemented support for edge-to-edge enforcement;

--- app/src/main/java/xyz/teamgravity/imageradiobuttondemo/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/imageradiobuttondemo/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import xyz.teamgravity.imageradiobutton.GravityImageRadioButton
 import xyz.teamgravity.imageradiobuttondemo.databinding.ActivityMainBinding
 
@@ -12,10 +14,17 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { view, insets ->
+            val paddings =
+                insets.getInsets(WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.displayCutout() or WindowInsetsCompat.Type.ime())
+            view.setPadding(paddings.left, paddings.top, paddings.right, paddings.bottom)
+            return@setOnApplyWindowInsetsListener insets
+        }
 
         binding.gravityGroup.setOnCheckedChangeListener { _, radioButton, _, _ ->
             Toast.makeText(this@MainActivity, (radioButton as GravityImageRadioButton).text(), Toast.LENGTH_SHORT).show()
